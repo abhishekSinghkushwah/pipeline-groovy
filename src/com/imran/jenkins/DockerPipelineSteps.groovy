248477e3ab2ff170ba98ec9f4e7e77b0e0fe5960
imshaik <shaik.imran@lowes.com>
2018-03-01 17:12:37 -0500

    Updated dockerpipeline script to remove images and containers

--- src/com/imran/jenkins/DockerPipelineSteps.groovy
+++ src/com/imran/jenkins/DockerPipelineSteps.groovy
@@ -7,8 +7,17 @@ class DockerPipelineSteps implements Serializable {
   DockerPipelineSteps(steps) { this.steps = steps }
 
   def RemoveImage() {
-    def command = "docker rmi -f $(docker images | grep <none> | tr -s ' ' | cut -d ' ' -f3)"
+	def Imageid = steps.sh (
+	script: "docker images | grep '<none>' | tr -s ' ' | cut -d ' ' -f3",
+	returnStdout: true
+        ).trim()
+    def command = "docker rmi -f ${Imageid}"
       steps.sh "${command}"
+    }
+
+  def RemoveContainer() {
+	def ContainerId = "docker ps -a -q | xargs -n 1 -I {} sudo docker rm -f {}"
+        steps.sh "${ContainerId}"
     }
   }
 
