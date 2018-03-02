93f0d04f2053298d1ffcd5112eecbcd374fde01a
imshaik <shaik.imran@lowes.com>
2018-03-02 16:54:52 -0500

    Remove image script added

--- src/com/imran/jenkins/DockerPipelineSteps.groovy
+++ src/com/imran/jenkins/DockerPipelineSteps.groovy
@@ -6,14 +6,19 @@ class DockerPipelineSteps implements Serializable {
 
   DockerPipelineSteps(steps) { this.steps = steps }
 
-  def RemoveImage() {
-	def Imageid = steps.sh (
+  def RemoveNoneImage() {
+	def NoneImageid = steps.sh (
 	script: "docker images | grep '<none>' | tr -s ' ' | cut -d ' ' -f3",
 	returnStdout: true
         ).trim()
-    def command = "docker rmi -f ${Imageid}"
+    def command = "docker rmi -f ${NoneImageid}"
       steps.sh "${command}"
     }
+
+  def RemoveImage(dockerreponame, buildlabel) {
+	def Imageid = "docker rmi -f shaikimranashrafi/dockerreponame:buildlabel"
+	steps.sh "${Imageid}"
+	}
 
   def RemoveContainer() {
 	def ContainerId = "docker ps -a -q | xargs -n 1 -I {} sudo docker rm -f {}"
