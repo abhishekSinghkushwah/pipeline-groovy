f6ce248a252c0ae3575e50026398c721b2bca070
imshaik <shaik.imran@lowes.com>
2018-05-11 16:03:25 -0400

    Modified the script

--- src/com/imran/jenkins/DockerPipelineSteps.groovy
+++ src/com/imran/jenkins/DockerPipelineSteps.groovy
@@ -18,9 +18,9 @@ class DockerPipelineSteps implements Serializable {
                 currentBuild.result = 'FAILURE'
           throw err
         } finally { 
-	 /* step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/*.xml', healthScaleFactor: 1.0])
-	  junit '**/target/surefire-reports/*.txt'*/
-	  archiveTestReport(testreport)
+	 // step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/*.xml', healthScaleFactor: 1.0])
+	  //  junit '**/target/surefire-reports/*.txt'
+	    archiveTestReport(testreport)
        }
    }
 
