ad1949842941e46a7ec496292d7d7492fb822603
imshaik <shaik.imran@lowes.com>
2018-12-06 17:39:38 -0500

    removed steps.

--- src/com/imran/jenkins/Nexus.groovy
+++ src/com/imran/jenkins/Nexus.groovy
@@ -17,8 +17,8 @@ println "Application: ${app}  Tag: ${tag} "
 def url = "https://docker.imran.com:18443/v2/${app}/manifests/${tag}"
 def Get_Sha = steps.sh(script: "curl -I -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -X GET ${url} | grep Docker-Content-Digest | awk '{print \$2}' ", returnStdout: true).trim()
 
-println "Fetched sha :" + Get_Sha
-    return steps.Get_Sha
+  println "Fetched sha :" + Get_Sha
+    return Get_Sha
     //return this.Tag_Sha
 }
 
