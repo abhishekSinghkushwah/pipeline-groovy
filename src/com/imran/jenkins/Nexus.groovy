52b081d28046c367654c204951ffbbb35ce9e825
imshaik <shaik.imran@lowes.com>
2018-12-11 17:08:46 -0500

    Adding script for img digest to return

--- src/com/imran/jenkins/Nexus.groovy
+++ src/com/imran/jenkins/Nexus.groovy
@@ -12,13 +12,29 @@ public class Nexus implements Serializable {
     
 def get_sha_for_tags(app, tag ) {
     
-println "Application: ${app}  Tag: ${tag} "
+println "Application is : ${app} and  Tag is : ${tag} "
 
 def url = "https://docker.imran.com:18443/v2/${app}/manifests/${tag}"
 def Get_Sha = steps.sh(script: "curl -I -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -X GET ${url} | grep Docker-Content-Digest | awk '{print \$2}' ", returnStdout: true).trim()
 
-  println "Fetched sha :" + Get_Sha
-    return Get_Sha
+println "Fetched sha :" + Get_Sha
+
+println 
+def imgurl = "https://docker.imran.com:18443/v2/${app}/manifests/${Get_Sha}"
+
+//def url = "https://docker.imran.com:18443/v2/"+ app +"/manifests/" + sha + ""
+
+def json = new JsonSlurper().parseText(imgurl.toURL().getText(
+  requestProperties: [
+   'Accept': 'application/json'
+  ]
+));
+
+def Getimgdigest = json.config.digest.trim();
+
+
+return Get_Sha
+return Getimgdigest
     //return this.Tag_Sha
 }
 
